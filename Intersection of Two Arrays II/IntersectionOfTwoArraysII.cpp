400e7be5ba7d75bac23814d5d8608bf332830ed7
Michael Slice <mjsens523@gmail.com>
2023-08-29 15:26:43 -0500

    Added Solution for Intersection of Two Arrays II

---
+++ Intersection of Two Arrays II/IntersectionOfTwoArraysII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+
+    std::vector<int> vec;
+
+    for(int i{0}; i < nums1.size(); ++i)
+    {
+        for(int j{0}; j < nums2.size(); ++j)
+        {
+            if(nums1[i] == nums2[j])
+            {
+                vec.push_back(nums1[i]);
+                    nums2[j] =-1;
+                break;
+            }
+        }
+    }
+return vec;
+
+    }
+};
