5f28000617dfbd5f485f934c508655d79f8e4fd9
Michael Slice <mjsens523@gmail.com>
2023-09-06 19:46:42 -0500

    Added Solution for Problem 2089

---
+++ Arrays/2089. Find Target Indices After Sorting Array/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+
+    std::sort(nums.begin(), nums.end());
+    vector<int> vec;
+   
+    for(int i={0}; i < nums.size(); ++i)
+    {
+        if (nums[i] == target) {
+            vec.push_back(i);
+        }
+    }
+return vec;
+    }
+};
